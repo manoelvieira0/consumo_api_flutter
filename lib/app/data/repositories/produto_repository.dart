7daebbe328d252df06cf05e2954b8947d93830eb
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-17 07:05:49 -0300

    Requisição da api

--- lib/app/data/repositories/produto_repository.dart
+++ lib/app/data/repositories/produto_repository.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:consumo_api_flutter/app/data/http/exceptions.dart';
+import 'package:consumo_api_flutter/app/data/http/http_client.dart';
 import 'package:consumo_api_flutter/app/data/models/product_model.dart';
 
 abstract class IProdutoRepository {
@@ -5,10 +9,31 @@ abstract class IProdutoRepository {
 }
 
 class ProdutocRepository implements IProdutoRepository {
+
+  final IHttpClient client;
+
+  ProdutocRepository({required this.client});
+
   @override
-  Future<List<ProductModel>> getProdutos() {
-    // TODO: implement get
-    throw UnimplementedError();
+  Future<List<ProductModel>> getProdutos() async {
+    final response = await client.get(url: 'https://dummyjson.com/products');
+
+    if (response.statusCode == 200){
+      final List<ProductModel> products = []; // Criando uma lista vazia
+
+      final body = jsonDecode(response.body); // Decodificando Json 
+
+      body['products'].map((item){ // Mapeando a lista
+        final ProductModel product = ProductModel.fromMap(item);
+        products.add(product);
+      }).toList();
+
+      return products;
+    } else if (response.statusCode == 404) {
+      throw NotFoundException("A URL Informada não é válida.");
+    } else {
+      throw Exception("Não foi possível carregar os produtos.");
+    }
   }
   
 }
