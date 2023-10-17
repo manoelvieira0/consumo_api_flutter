298d2149e92e696077f8f12e571a99eda70bc806
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-17 06:31:54 -0300

    Criando projeto flutter

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:consumo_api_flutter/app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:consumo_api_flutter/app/app_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
