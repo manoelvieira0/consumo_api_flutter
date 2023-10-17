8eb3700058f67dcb0ce10c21156027dcff50102c
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-17 06:41:43 -0300

    Criando Model

---
+++ lib/app/data/models/product_model.dart
@@ -0,0 +1,21 @@
+class ProductModel {
+  final String title;
+  final String description;
+  final double price;
+  final double rating;
+  final String brand;
+  final String category;
+  final String thumbnail;
+  final List<String> images;
+
+  ProductModel({
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.rating,
+    required this.brand,
+    required this.category,
+    required this.thumbnail,
+    required this.images,
+  });
+}
