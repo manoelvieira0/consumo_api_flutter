b8377c9be5a61db5fa5bbe1c16f43988d5b3b346
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-17 07:32:42 -0300

    Exibindo API

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -19,6 +19,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    store.getProdutos();
   }
 
   @override
@@ -34,7 +35,97 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Container(),
+      body: AnimatedBuilder(
+          animation: Listenable.merge([
+            store.isLoading,
+            store.erro,
+            store.state,
+          ]),
+          builder: (context, child) {
+            if (store.isLoading.value) {
+              return const CircularProgressIndicator();
+            }
+
+            if (store.erro.value.isNotEmpty) {
+              return Center(
+                  child: Text(
+                store.erro.value,
+                style: const TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ));
+            }
+
+            if (store.state.value.isEmpty) {
+              return const Center(
+                child: Text(
+                  'Nenhum item na lista',
+                  style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else {
+              return ListView.separated(
+                separatorBuilder: (context, index) => const SizedBox(
+                  height: 32,
+                ),
+                padding: const EdgeInsets.all(16),
+                itemCount: store.state.value.length,
+                itemBuilder: (_, index) {
+                  final item = store.state.value[index];
+                  return Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.network(
+                          item.thumbnail,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      ListTile(
+                        contentPadding: EdgeInsets.zero,
+                        title: Text(
+                          item.title,
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 24),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'R\$ ${item.price}',
+                              style: const TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 20),
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              item.description,
+                              style: const TextStyle(
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 18),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  );
+                },
+              );
+            }
+          }),
     );
   }
 }
