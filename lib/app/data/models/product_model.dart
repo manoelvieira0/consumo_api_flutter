b87b2510d887d2808be1675fb288efddddb10c03
Manoel Vieira <netinhovieira1801@gmail.com>
2023-10-17 06:48:31 -0300

    Finalizando Model com Map

--- lib/app/data/models/product_model.dart
+++ lib/app/data/models/product_model.dart
@@ -18,4 +18,18 @@ class ProductModel {
     required this.thumbnail,
     required this.images,
   });
+
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    //Chave é String e Recebe valores dinâmicos
+    return ProductModel(
+      title: map['title'],
+      description: map['description'],
+      price: map['price'] * 1.0, // Para transformar em double
+      rating: map['rating'] * 1.0,
+      brand: map['brand'],
+      category: map['category'],
+      thumbnail: map['thumbnail'],
+      images: List<String>.from((map['images'] as List)), // Recebe lista de Strings
+    );
+  }
 }
